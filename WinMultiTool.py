b6141c0b04b8d8edcc4dbaed10ef9a9aa965055e
TheBigDawgStepper <thebigdawgstepper@protonmail.com>
2024-05-21 00:25:00 -0400

    Update WinMultiTool.py
    Just doing more fixes and completing features

--- WinMultiTool.py
+++ WinMultiTool.py
@@ -9,17 +9,16 @@ def open_task_manager():
     except Exception as e:
         print(f"An error occured: {e}")
 
-
 def multiple_choice():
-    print("Make the choice of which tool ur tryna use")
     print("1. Open Task Manager")
     print("2. Open cmd")
     print("3. Open System Info")
     print("4. Work in progress")
     print("5. Work in progress")
-if choice == 1:
-    print("Opening Task Manager.")
+    choice = int(input("Choose a tool by typing the number of which you want: "))
+    if choice == 1:
+     print("Opening Task Manager.")
+     open_task_manager()
     
 if __name__ == "__main__":
     multiple_choice()
-    open_task_manager()
