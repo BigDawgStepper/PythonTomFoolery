45c1fa71301eae72138ac5b9c1e8e75e55b46b77
TheBigDawgStepper <thebigdawgstepper@protonmail.com>
2024-05-21 01:23:57 -0400

    Update WinMultiTool.py
    1.) Added a time function for some delay so printing text wouldnt look so fucked.
    2.) Added a system info tool.
    3.) Added a control panel tool.

--- WinMultiTool.py
+++ WinMultiTool.py
@@ -1,19 +1,36 @@
 import os
+import time
 
 print("Welcome to BigDawgSteppers Windows Multi Tool")
 
-def open_calculator():
+def open_control_panel():
     try:
-        os.system("start calc")
-        print("Calculator is open")
+        os.system("start control")
+        print("Control Panel Is Running!")
+    except Exception as e:
+        print("An error has occured: {e}")
+
+def open_system_info():
+    try:
+        time.sleep(3)
+        os.system("start msinfo32")
+        print("System Information is Running!")
     except Exception as e:
         print("An error occured: {e}")
 
+def open_calculator():
+    try:
+        time.sleep(3)
+        os.system("start calc")
+        print("Calculator is open!")
+    except Exception as e:
+        print("An error occured: {e}")
 
 def open_task_manager():
     try: 
+        time.sleep(3)
         os.system("start taskmgr")
-        print("Task manager should be opened")
+        print("Task manager should be opened!")
     except Exception as e:
         print(f"An error occured: {e}")
 
@@ -21,7 +38,7 @@ def multiple_choice():
     print("1. Open Task Manager")
     print("2. Open Calculator")
     print("3. Open System Info")
-    print("4. Work in progress")
+    print("4. Open Control Panel")
     print("5. Work in progress")
     choice = int(input("Choose a tool by typing the number of which you want: "))
     if choice == 1:
@@ -32,6 +49,14 @@ def multiple_choice():
         print("Opening Calculator")
         open_calculator()
         input("Press enter to quit...")
-  
+    elif choice == 3:
+        print("Opening System Information")
+        open_system_info()
+        input("Press enter to quit...")
+    elif choice == 4:
+        print("Opening Control Panel")
+        open_control_panel()
+        input("Press enter to quit...")
+ 
 if __name__ == "__main__":
     multiple_choice()
