bc00905e038416ef3a2fd42119d5766b07fa9c79
TheBigDawgStepper <thebigdawgstepper@protonmail.com>
2024-05-21 01:37:42 -0400

    Update WinMultiTool.py
    1.) Added file explorer tool

--- WinMultiTool.py
+++ WinMultiTool.py
@@ -2,9 +2,21 @@ import os
 import time
 
 print("Welcome to BigDawgSteppers Windows Multi Tool")
+time.sleep(2)
+print("Loading...")
+time.sleep(3)
+
+def open_file_explorer():
+    try:
+        time.sleep(2)
+        os.system("start explorer")
+        print("File Explorer is Running!")
+    except Exception as e:
+        print("An error occured: {e}")
 
 def open_control_panel():
     try:
+        time.sleep(2)
         os.system("start control")
         print("Control Panel Is Running!")
     except Exception as e:
@@ -12,7 +24,7 @@ def open_control_panel():
 
 def open_system_info():
     try:
-        time.sleep(3)
+        time.sleep(2)
         os.system("start msinfo32")
         print("System Information is Running!")
     except Exception as e:
@@ -20,7 +32,7 @@ def open_system_info():
 
 def open_calculator():
     try:
-        time.sleep(3)
+        time.sleep(2)
         os.system("start calc")
         print("Calculator is open!")
     except Exception as e:
@@ -28,7 +40,7 @@ def open_calculator():
 
 def open_task_manager():
     try: 
-        time.sleep(3)
+        time.sleep(2)
         os.system("start taskmgr")
         print("Task manager should be opened!")
     except Exception as e:
@@ -39,7 +51,7 @@ def multiple_choice():
     print("2. Open Calculator")
     print("3. Open System Info")
     print("4. Open Control Panel")
-    print("5. Work in progress")
+    print("5. Open File Explorer")
     choice = int(input("Choose a tool by typing the number of which you want: "))
     if choice == 1:
      print("Opening Task Manager.")
@@ -56,6 +68,10 @@ def multiple_choice():
     elif choice == 4:
         print("Opening Control Panel")
         open_control_panel()
+        input("Press enter to quit...")
+    elif choice == 5:
+        print("Opening File Explorer")
+        open_file_explorer()
         input("Press enter to quit...")
  
 if __name__ == "__main__":
