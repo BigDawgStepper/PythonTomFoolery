1c4e668721f461d4319b0c87ee5096d16f546652
TheBigDawgStepper <thebigdawgstepper@protonmail.com>
2024-05-21 01:05:54 -0400

    Update WinMultiTool.py

--- WinMultiTool.py
+++ WinMultiTool.py
@@ -2,6 +2,14 @@ import os
 
 print("Welcome to BigDawgSteppers Windows Multi Tool")
 
+def open_calculator():
+    try:
+        os.system("start calc")
+        print("Calculator is open")
+    except Exception as e:
+        print("An error occured: {e}")
+
+
 def open_task_manager():
     try: 
         os.system("start taskmgr")
@@ -11,7 +19,7 @@ def open_task_manager():
 
 def multiple_choice():
     print("1. Open Task Manager")
-    print("2. Open cmd")
+    print("2. Open Calculator")
     print("3. Open System Info")
     print("4. Work in progress")
     print("5. Work in progress")
@@ -19,6 +27,11 @@ def multiple_choice():
     if choice == 1:
      print("Opening Task Manager.")
      open_task_manager()
-    
+     input("Press enter to quit...")
+    elif choice == 2:
+        print("Opening Calculator")
+        open_calculator()
+        input("Press enter to quit...")
+  
 if __name__ == "__main__":
     multiple_choice()
